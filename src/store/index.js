4773db22f32741959a3f42243667cca2fbe3da55
Awtian <me@awtian.com>
2018-10-31 14:18:02 +0700

    add thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux'; 
+import { createStore, combineReducers, applyMiddleware } from 'redux'; 
+import thunk from 'redux-thunk'
 import Login from '../reducers/login'
 import Name from '../reducers/name'
 import person from '../reducers/person'
@@ -7,6 +8,6 @@ const reducers = combineReducers({
   Login, Name, person
 })
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
